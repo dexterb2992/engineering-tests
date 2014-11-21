2073b46c293adc967e2e59c1ff9cbff3370e9e02
Dexter Bengil <dexterb2992@gmail.com>
2014-11-21 12:13:45 +0800

    database config

--- testing-app/app/config/database.php
+++ testing-app/app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'testing',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => '127.0.0.1',
+			'database'  => 'restaurant-offer',
+			'username'  => 'restaurant-offer',
+			'password'  => 'hIHhFzdG2xlSAS_H',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
